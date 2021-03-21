71c3472a09a1583d1af93f2c5a8f19157e9235a5
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-03-21 22:01:11 +0100

    Completed the Node Farm Project

--- httpModule.js
+++ httpModule.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // HTML TEMPLATE
 const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
@@ -11,20 +13,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-// REPLACE TEMPLATE
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{% PRODUCTNAME %}/g, product.productName);
-    output = output.replace(/{% IMAGE %}/g, product.image);
-    output = output.replace(/{% PRICE %}/g, product.price);
-    output = output.replace(/{% FROM %}/g, product.from);
-    output = output.replace(/{% NUTRIENTS %}/g, product.nutrients);
-    output = output.replace(/{% QUANTITY %}/g, product.quantity);
-    output = output.replace(/{% DESCRIPTION %}/g, product.description);
-    output = output.replace(/{% ID %}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{% NOT_ORGANIC %}/g, 'not-organic');
-    return output;
-};
+// This only shows us how slugify works.
+// const slugs = dataObj.map(element => slugify(element.productName, { lower: true }));
+// console.log(slugs);
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);

37cbcac7d071e860c40bf76d4b2cfe3ba8b95de4
Alexander Nitiola <cre8tor.alexander@gmail.com>
2021-03-21 13:37:26 +0100

    Learnt how to create a route in node-js

--- httpModule.js
+++ httpModule.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    return res.end('Hello from the server');
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        return res.end('This is the OVERVIEW page');
+    } else if (pathName === '/product') {
+        return res.end('This is the PRODUCT page');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        return res.end('<h1>404 This page does not exist</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
